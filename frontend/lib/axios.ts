43942a30d8fe3c39dcaa416af09b15e0ac2c5bb6
shamlan27 <shamlanm08@gmail.com>
2025-12-25 15:06:10 +0530

    feat: Add configured Axios instance for API requests.

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     headers: {
         'Accept': 'application/json',
     },
