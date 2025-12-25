5d9d46236fe91800ecc37929f1d8b17eeb219f05
shamlan27 <shamlanm08@gmail.com>
2025-12-25 13:14:19 +0530

    refactor: Restructure project by moving Laravel files to `backend/` and adding `frontend/` assets.

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -3,10 +3,9 @@ import axios from 'axios';
 const axiosInstance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
     headers: {
-        'X-Requested-With': 'XMLHttpRequest',
         'Accept': 'application/json',
     },
-    withCredentials: true,
+
 });
 
 export default axiosInstance;
