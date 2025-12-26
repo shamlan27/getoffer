cbb4510bbc9d7b46ac1e0436b312aef88af15254
shamlan27 <shamlanm08@gmail.com>
2025-12-26 17:09:56 +0530

    feat: add debug route to inspect Cloudinary and filesystem configurations.

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -6,3 +6,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/debug-config', function () {
+    // 1. Force Clear Cache
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+
+    // 2. Show us the LIVE config to prove if 'key' exists
+    $cloudinaryConfig = config('cloudinary.cloud');
+    $filesystemConfig = config('filesystems.disks.cloudinary');
+
+    return [
+        '1_cache_status' => 'CLEARED',
+        '2_cloudinary_config_check' => [
+            'has_api_key' => isset($cloudinaryConfig['api_key']),
+            'has_key_alias' => isset($cloudinaryConfig['key']), // <--- WE NEED THIS TO BE TRUE
+            'values' => $cloudinaryConfig,
+        ],
+        '3_filesystem_config_check' => [
+            'has_api_key' => isset($filesystemConfig['cloud']['api_key']),
+            'has_key_alias' => isset($filesystemConfig['cloud']['key']), // <--- WE NEED THIS TO BE TRUE
+            'values' => $filesystemConfig,
+        ]
+    ];
+});
