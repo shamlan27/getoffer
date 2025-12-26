316d480eac24bc43c9608b7ca05ccdea8f0de1e1
shamlan27 <shamlanm08@gmail.com>
2025-12-26 16:29:04 +0530

    feat: add Cloudinary configuration file.

--- backend/config/cloudinary.php
+++ backend/config/cloudinary.php
@@ -21,6 +21,14 @@ return [
     */
     'notification_url' => env('CLOUDINARY_NOTIFICATION_URL'),
 
+
+
+    'cloud' => [
+        'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+        'api_key'    => env('CLOUDINARY_API_KEY', env('CLOUDINARY_KEY')),
+        'api_secret' => env('CLOUDINARY_API_SECRET', env('CLOUDINARY_SECRET')),
+    ],
+    
     /*
     |--------------------------------------------------------------------------
     | Cloudinary Configuration
