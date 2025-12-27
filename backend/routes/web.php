0dec5f8271402b0618ddf933546b4a3145b96f3d
shamlan27 <shamlanm08@gmail.com>
2025-12-27 17:10:06 +0530

    feat: add web routes for welcome page and Brevo template listing

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -6,3 +6,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/list-templates', function () {
+    return \Illuminate\Support\Facades\Http::withHeaders([
+        'api-key' => env('BREVO_API_KEY'),
+        'accept' => 'application/json'
+    ])->get('https://api.brevo.com/v3/smtp/templates')->json();
+});
